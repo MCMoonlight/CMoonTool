0fea2cfc564de7997c23245187d90659f5b1846c
FSRH <3565723760@QQ.com>
2023-12-24 19:44:01 +0800

    111

---
+++ MoonTool.h
@@ -0,0 +1,15 @@
+#ifndef MO_MOON_H
+#define MO_MOON_H
+
+#include <inttypes.h>     
+#include <assert.h>         
+#include <limits.h>              
+#include <math.h>                 
+#include <stdarg.h>               
+#include <wchar.h>                
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>          
+#endif
+
+
+#endif //MO_MOON_H
